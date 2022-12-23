39c63edc1945abb16caaf822897d09a745e26326
JulinIngS <anaelmirey@gmail.com>
2022-12-22 21:14:05 -0500

    Web para C

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(cambiaMensaje,4500)
+
+function cambiaMensaje(){
+    document.getElementById("loading-message").innerText="Abrazo Enviado a la más guapa! Osea tú";
+}
+
+setTimeout(function () {
+    alert("Recibiste un abrazo, del chico más guapo, osea Julito")
+},4550)
